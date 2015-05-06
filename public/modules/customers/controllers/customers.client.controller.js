a9ef7ed8a6650dce5b98400aa4f1cb17e2be3c6c
christophechevalier <christophe.chevalier1@viacesi.fr>
2015-05-07 01:33:29 +0200

    Test 1 (next) - Working Update Modal Window

--- public/modules/customers/controllers/customers.client.controller.js
+++ public/modules/customers/controllers/customers.client.controller.js
@@ -4,14 +4,48 @@
 
 var customerApp = angular.module('customers');
 
-customerApp.controller('CustomersController', ['$scope', '$stateParams', 'Authentication', 'Customers',
-	function($scope, $stateParams, Authentication, Customers) {
+customerApp.controller('CustomersController', ['$scope', '$stateParams', 'Authentication', 'Customers', '$modal', '$log',
+	function($scope, $stateParams, Authentication, Customers, $modal, $log) {
 
 		this.authentication = Authentication;
 
-
 		// Find a list of Customers
-		$scope.customers = Customers.query();
+		this.customers = Customers.query();
+        
+        // Open a modal window to Update a single customer record
+        this.modalUpdate = function (size, selectedCustomer) {
+
+            var modalInstance = $modal.open({
+                templateUrl: 'modules/customers/views/edit-customer.client.view.html',
+                controller: function ($scope, $modalInstance, customer) {
+                    $scope.customer = customer;
+                    
+                    $scope.ok = function () {
+                        
+                        if (updateCustomerForm.valid){
+                            $modalInstance.close($scope.customer);                            
+                        }
+
+                    };
+
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                },
+                size: size,
+                resolve: {
+                    customer: function () {
+                        return selectedCustomer;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
 	}
 ]);
 
@@ -21,9 +55,19 @@ customerApp.controller('CustomersCreateController', ['$scope', 'Customers',
 	}
 ]);
 
-customerApp.controller('CustomersEditController', ['$scope', 'Customers',
+customerApp.controller('CustomersUpdateController', ['$scope', 'Customers',
 	function($scope, Customers) {
+        
+		// Update existing Customer
+		this.update = function(updatedCustomer) {
+			var customer = updatedCustomer;
 
+			customer.$update(function() {
+
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
 	}
 ]);
 
