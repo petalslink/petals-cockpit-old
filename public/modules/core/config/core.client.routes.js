d5e2e9efad1396070167c950bf04e0f2a69f2b92
Christophe Chevalier <christophe.chevalier1@viacesi.fr>
2015-06-12 17:09:18 +0200

    Petals Cockpit Structure ok -
    Nav Tree not return object

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -5,15 +5,16 @@
 var dependencies = ['ntt.TreeDnD'];
 
 angular.module('core', dependencies).config(['$stateProvider', '$urlRouterProvider',
-	function($stateProvider, $urlRouterProvider) {
-		// Redirect to home view when route not found
-		$urlRouterProvider.otherwise('/');
+    function ($stateProvider, $urlRouterProvider) {
+        // Redirect to home view when route not found
+        $urlRouterProvider.otherwise('/');
 
-		// Home state routing
-		$stateProvider.
-		state('home', {
-			url: '/',
-			templateUrl: 'modules/core/views/home.client.view.html'
-		});
-	}
+        // Home state routing
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'modules/core/views/home.client.view.html'
+            });
+    }
 ]);
+
