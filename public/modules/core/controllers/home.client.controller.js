7f9334729f5101fb58fe61abbc334ec609de28a9
Christophe Chevalier <christophe.chevalier1@viacesi.fr>
2015-07-09 19:30:33 +0200

    Build Nav Tree Workspace with custom css -

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -8,8 +8,6 @@ app.controller('HomeController', ['$scope', 'Authentication',
         // This provides Authentication context.
         $scope.authentication = Authentication;
 
-        $scope.components = {};
-
         /*        $scope.alerts = [
          {
          icon: 'glyphicon-globe',
@@ -39,100 +37,105 @@ app.controller('HomeController', ['$scope', 'Authentication',
     }
 ]);
 
-app.controller('treeCtrl', ['$q', '$scope', '$timeout', '$TreeDnDConvert', 'Buses', 'Nodes', 'Components', 'Serviceunits', 'Services',
-        function ($q, $scope, $timeout, $TreeDnDConvert, Buses, Nodes, Components, Serviceunits, Services) {
+app.controller('treeCtrl', ['$q', '$scope', '$timeout', 'Buses', 'Nodes', 'Components', 'Serviceunits', 'Services',
+        function ($q, $scope, $timeout, Buses, Nodes, Components, Serviceunits, Services) {
 
-            $scope.my_tree = {};
-
-            $scope.my_tree.addFunction = function(b){
-                console.log(b);
-                alert('Function added in Controller "App.js"');
-            };
-            $scope.$callbacks = {};
-            $scope._filter = {};
-            $scope.tree_nodes = [];
-            $scope.tree_data = {};
-            $scope.expanding_property = {
-                field: 'title',
-                titleClass: 'text-left',
-                cellClass: 'v-middle',
-                displayName: 'Name WorkSpace'
-            };
-/*            $scope.col_defs = [
-                {
-                    field: "Description"
-                }, {
-                    displayName:  'Function',
-                    cellTemplate: '<button ng-click="tree.addFunction(node)" class="btn btn-default btn-sm">Added Controller!</button>'
-                }];*/
-            $scope.select_handler = function (node) {
-            };
-            $scope.click_handler = function (node) {
-            };
-
-            
             // VZ
-            
+
             $q.all([
                 Buses.getBuses().$promise,
                 Nodes.getNodes().$promise,
                 Components.getComponents().$promise,
                 Serviceunits.getServiceunits().$promise,
                 Services.getServices().$promise
-            ]).then( buildTree );
-            
-            
-            function buildTree( data ) {
-              
-              var roots = [];
+            ]).then(buildTree);
 
-              // Deal with buses
-              var bindex = {};
-              data[0].forEach( function(val, index, arr) {
-                var bus = {
-                  _id: val._id,
-                  name: val.name,
-                  children: []
-                };
-                
-                bindex[val._id] = bus;
-                roots.push(bus);
-              });
 
-              // Deal with nodes
-              var nindex = {};
-              console.log(data[1]);
-              data[1].forEach( function(val, index, arr) {
-                var node = {
-                  _id: val._id,
-                  name: val.name,
-                  children: [],
-                  parent: bus
-                };
-                
-                nindex[val._id] = node;
-                var bus = bindex[val.parentBus._id];
-                bus.children.push(node);
-              });
+            function buildTree(data) {
 
-              // Deal with components
-              var cindex = {};
-              data[2].forEach( function(val, index, arr) {
-                var component = {
-                  _id: val._id,
-                  name: val.name,
-                  children: [],
-                  parent: node
-                };
-                
-                cindex[val._id] = val;
-                var node = nindex[val.parentServer._id];
-                node.children.push(component);
-              });
-              
-              // Register the tree in the scope
-              $scope.rootNodes = roots;
+                var roots = [];
+
+                // Deal with buses
+                var bindex = {};
+                data[0].forEach(function (val, index, arr) {
+                    var bus = {
+                        _id: val._id,
+                        name: val.name,
+                        children: [],
+                        icon: 'glyphicon-flash'
+                    };
+
+                    bindex[val._id] = bus;
+                    roots.push(bus);
+                });
+
+                // Deal with nodes -> console.log(data[1]);
+                var nindex = {};
+                data[1].forEach(function (val, index, arr) {
+                    var node = {
+                        _id: val._id,
+                        name: val.name,
+                        children: [],
+                        icon: 'glyphicon-wrench',
+                        parent: bus
+                    };
+
+                    nindex[val._id] = node;
+                    var bus = bindex[val.parentBus._id];
+                    bus.children.push(node);
+                });
+
+                // Deal with components
+                var cindex = {};
+                data[2].forEach(function (val, index, arr) {
+                    var component = {
+                        _id: val._id,
+                        name: val.name,
+                        children: [],
+                        icon: 'glyphicon-tasks',
+                        parent: node
+                    };
+
+                    cindex[val._id] = component;
+                    var node = nindex[val.parentServer._id];
+                    node.children.push(component);
+                });
+
+                // Deal with serviceunits
+                var suindex = {};
+                data[3].forEach(function (val, index, arr) {
+                    var serviceunit = {
+                        _id: val._id,
+                        name: val.name,
+                        children: [],
+                        icon: 'glyphicon-pushpin',
+                        parent: component
+                    };
+
+                    suindex[val._id] = serviceunit;
+                    var component = cindex[val.parentComponent._id];
+                    component.children.push(serviceunit);
+                });
+
+                // Deal with services
+                var sindex = {};
+                data[4].forEach(function (val, index, arr) {
+                    var service = {
+                        _id: val._id,
+                        name: val.name,
+                        children: [],
+                        icon: 'glyphicon-sort',
+                        parent: serviceunit
+                    };
+
+                    sindex[val._id] = service;
+                    var serviceunit = suindex[val.parentServiceunit._id];
+                    serviceunit.children.push(service);
+                });
+
+                // Register the tree in the scope
+                $scope.rootNodes = roots;
             }
-            
+
         }]
 );
