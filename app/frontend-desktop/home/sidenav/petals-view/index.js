a85092fc2433fae8e21f0c97decad6a59cd95ef6
Christophe Chevalier <christophe.chevalier1@viacesi.fr>
2016-02-02 11:32:14 +0100

    Switch to modular organisation
    - Delete 2 pages config

--- app/frontend-desktop/home/sidenav/petals-view/index.js
+++ app/frontend-desktop/home/sidenav/petals-view/index.js
@@ -2,5 +2,19 @@
 // Petals View
 module.exports = angular.module('frontend-desktop.home.sidenav.petals-view', [])
     .directive('petalsView', require('./petalsDirective'))
-    .controller('PetalsCtrl', require('./PetalsController'));
-/*    .config('petalsConfig', require('./petalsConfig'));*/
+    .controller('PetalsCtrl', require('./PetalsController'))
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('petals-theme', 'default')
+            .primaryPalette('orange', {
+                'default': '800',
+                'hue-1': '800',
+                'hue-2': '300',
+                'hue-3': '100'
+            })
+            .accentPalette('deep-purple', {
+                'default': '800',
+                'hue-1': '800',
+                'hue-2': '300',
+                'hue-3': '200'
+            });
+    });
