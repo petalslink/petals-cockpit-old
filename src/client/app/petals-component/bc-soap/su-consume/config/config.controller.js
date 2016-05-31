09e2c75507fdd583b7ebbed7d755294a840a2b09
Christophe Chevalier <cchevalier@linagora.com>
2016-05-31 17:49:02 +0200

    Create Cdk_part & Soap_part for bc-soap-su-consume - Complete tiles & modals build

--- src/client/app/petals-component/bc-soap/su-consume/config/config.controller.js
+++ src/client/app/petals-component/bc-soap/su-consume/config/config.controller.js
@@ -149,6 +149,240 @@
                             ]
                         }
                     ]
+                },
+                /* Cdk_part */
+                {
+                    span: {row: 2, col:2},
+                    background: 'green',
+                    title: 'Cdk Part',
+                    model: {
+                        mep: vm.details.cdk_part.mep,
+                        operation: vm.details.cdk_part.operation,
+                        timeout: vm.details.cdk_part.timeout
+                    },
+                    fieldsDisplay: [
+                        {
+                            key: 'mep',
+                            type: 'details',
+                            templateOptions: {label: 'Mep :'}
+                        },
+                        {
+                            key: 'operation',
+                            type: 'details',
+                            templateOptions: {label: 'Operation :'}
+                        },
+                        {
+                            key: 'timeout',
+                            type: 'details',
+                            templateOptions: {label: 'Timeout :'}
+                        }
+                    ],
+                    fieldsModal: [
+                        {
+                            elementAttributes: {
+                                layout: 'row',
+                                'layout-sm': 'column',
+                                'layout-xs': 'column'
+                            },
+                            fieldGroup: [
+                                {
+                                    key: 'mep',
+                                    className: 'flex-xs-100 flex-sm-100 flex-100',
+                                    type: 'input',
+                                    templateOptions: {label: 'Mep : '}
+                                },
+                                {
+                                    key: 'operation',
+                                    className: 'flex-xs-100 flex-sm-100 flex-100',
+                                    type: 'input',
+                                    templateOptions: {label: 'Operation : '}
+                                }
+                            ]
+                        },
+                        {
+                            elementAttributes: {
+                                layout: 'row',
+                                'layout-sm': 'column',
+                                'layout-xs': 'column'
+                            },
+                            fieldGroup: [
+                                {
+                                    key: 'timeout',
+                                    className: 'flex-xs-100 flex-sm-100 flex-100',
+                                    type: 'intInput',
+                                    templateOptions: {label: 'Timeout : '}
+                                }
+                            ]
+                        }
+                    ]
+                },
+                /* Soap_part */
+                {
+                    span: {row: 4, col:2},
+                    background: 'purple',
+                    title: 'Soap Part',
+                    model: {
+                        wsdl: vm.details.soap_part.wsdl,
+                        service_name: vm.details.soap_part.service_name,
+                        soap_action: vm.details.soap_part.soap_action,
+                        mode: vm.details.soap_part.mode,
+                        enable_compatibility_for: vm.details.soap_part.enable_compatibility_for,
+                        enable_http_transport: vm.details.soap_part.enable_http_transport,
+                        enable_https_transport: vm.details.soap_part.enable_https_transport,
+                        enable_jms_transport: vm.details.soap_part.enable_jms_transport,
+                        http_services_redirection: vm.details.soap_part.http_services_redirection
+                    },
+                    fieldsDisplay: [
+                        {
+                            key: 'wsdl',
+                            type: 'details',
+                            templateOptions: {label: 'Wsdl :'}
+                        },
+                        {
+                            key: 'service_name',
+                            type: 'details',
+                            templateOptions: {label: 'Service_name :'}
+                        },
+                        {
+                            key: 'soap_action',
+                            type: 'details',
+                            templateOptions: {label: 'Soap_action :'}
+                        },
+                        {
+                            key: 'mode',
+                            type: 'details',
+                            templateOptions: {label: 'Mode :'}
+                        },
+                        {
+                            key: 'enable_compatibility_for',
+                            type: 'details',
+                            templateOptions: {label: 'Enable_compatibility_for :'}
+                        },
+                        {
+                            key: 'enable_http_transport',
+                            type: 'details',
+                            templateOptions: {label: 'Enable_http_transport :'}
+                        },
+                        {
+                            key: 'enable_https_transport',
+                            type: 'details',
+                            templateOptions: {label: 'Enable_https_transport :'}
+                        },
+                        {
+                            key: 'enable_jms_transport',
+                            type: 'details',
+                            templateOptions: {label: 'Enable_jms_transport :'}
+                        },
+                        {
+                            key: 'http_services_redirection',
+                            type: 'details',
+                            templateOptions: {label: 'Http_services_redirection :'}
+                        }
+                    ],
+                    fieldsModal: [
+                        {
+                            elementAttributes: {
+                                layout: 'row',
+                                'layout-sm': 'column',
+                                'layout-xs': 'column'
+                            },
+                            fieldGroup: [
+                                {
+                                    key: 'wsdl',
+                                    className: 'flex-xs-100 flex-sm-100 flex-100',
+                                    type: 'input',
+                                    templateOptions: {label: 'Wsdl : '}
+                                },
+                                {
+                                    key: 'service_name',
+                                    className: 'flex-xs-100 flex-sm-100 flex-100',
+                                    type: 'input',
+                                    templateOptions: {label: 'Service_name : ', 'required': true}
+                                }
+                            ]
+                        },
+                        {
+                            elementAttributes: {
+                                layout: 'row',
+                                'layout-sm': 'column',
+                                'layout-xs': 'column'
+                            },
+                            fieldGroup: [
+                                {
+                                    key: 'soap_action',
+                                    className: 'flex-xs-100 flex-sm-100 flex-100',
+                                    type: 'intInput',
+                                    templateOptions: {label: 'Soap_action : '}
+                                },
+                                {
+                                    key: 'mode',
+                                    className: 'flex-xs-100 flex-sm-100 flex-100',
+                                    type: 'input',
+                                    templateOptions: {label: 'Mode : ', 'required': true}
+                                }
+                            ]
+                        },
+                        {
+                            elementAttributes: {
+                                layout: 'row',
+                                'layout-sm': 'column',
+                                'layout-xs': 'column'
+                            },
+                            fieldGroup: [
+                                {
+                                    key: 'enable_compatibility_for',
+                                    className: 'flex-xs-100 flex-sm-100 flex-100',
+                                    type: 'input',
+                                    templateOptions: {label: 'Enable_compatibility_for : '}
+                                },
+                                {
+                                    key: 'enable_http_transport',
+                                    className: 'flex-xs-100 flex-sm-100 flex-100',
+                                    type: 'switchCustom',
+                                    defaultValue: true,
+                                    templateOptions: {label: 'Enable_http_transport : ', theme: 'cardCustom-theme'}
+                                }
+                            ]
+                        },
+                        {
+                            elementAttributes: {
+                                layout: 'row',
+                                'layout-sm': 'column',
+                                'layout-xs': 'column'
+                            },
+                            fieldGroup: [
+                                {
+                                    key: 'enable_https_transport',
+                                    className: 'flex-xs-100 flex-sm-100 flex-100',
+                                    type: 'switchCustom',
+                                    defaultValue: false,
+                                    templateOptions: {label: 'Enable_https_transport : ', theme: 'cardCustom-theme'}
+                                },
+                                {
+                                    key: 'enable_jms_transport',
+                                    className: 'flex-xs-100 flex-sm-100 flex-100',
+                                    type: 'switchCustom',
+                                    defaultValue: false,
+                                    templateOptions: {label: 'Enable_jms_transport : ', theme: 'cardCustom-theme'}
+                                }
+                            ]
+                        },
+                        {
+                            elementAttributes: {
+                                layout: 'row',
+                                'layout-sm': 'column',
+                                'layout-xs': 'column'
+                            },
+                            fieldGroup: [
+                                {
+                                    key: 'http_services_redirection',
+                                    className: 'flex-xs-100 flex-sm-100 flex-100',
+                                    type: 'input',
+                                    templateOptions: {label: 'Http_services_redirection : '}
+                                }
+                            ]
+                        }
+                    ]
                 }
             ];
 
