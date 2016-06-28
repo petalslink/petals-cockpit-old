faa6340ff8f8ae779bffb7ac28563c11d9c452c4
Christophe Chevalier <cchevalier@linagora.com>
2016-06-28 11:40:08 +0200

    Create Models & Config Server folder

---
+++ src/server/config/db.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+    database: 'mongodb://localhost:27017/petals_cockpit_db',
+    secretKey: 'petalsCockpitAwesome'
+};
---
+++ src/server/models/database.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var environment = process.env.NODE_ENV;
+var configDB = require('../config/db'); // get db config file
+var dbURI = configDB.database;
+
+mongoose.connect(environment || dbURI, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connected to the petals_cockpit_db');
+    }
+});
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
+
+// BRING IN YOUR SCHEMAS & MODELS
+require('./account');
